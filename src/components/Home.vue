4bdc10e331bbff7d8cf6d63a57037b7ba2b975d9
Nestor A Hernandez <hernandez_nestor@network.lilly.com>
2019-08-04 14:32:26 -0400

    fixed vulnerablities and new background

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,39 +1,21 @@
 <template>
     <v-layout align-center column justify-center class="welcome">
-        <h1 class="display-2 font-weight-thin mb-3 font-weight-bold">{{welcome}}</h1>
+        <h1 class="display-2 font-weight-thin mb-3 font-weight-bold">Hello there,</h1>
         <h2 class="display-1 font-weight-thin mb-3 text-md-center text-sm-center font-weight-medium">
-            I am Nestor Hernandez <br><code>Developer</code> by day <code class="secondary black--text salsero-span">Salsero</code> by night
+            <h1>I'm Nestor</h1> <br><code>Developer</code> by day <code class="secondary black--text salsero-span">Salsero</code> by night
         </h2>
     </v-layout>
 
 </template>
 
 <script>
-const dynamicContent = {
-    spanish : {
-        welcome : 'Bienvenidos!'
-    },
-    english : {
-        welcome : 'Welcome!'
-    }
-}
 export default {
     name: 'home',
     mounted: function () {
-        window.setInterval(() => {
-            if (this.currentLanguage === 'english') {
-                this.welcome = dynamicContent.spanish.welcome
-                this.currentLanguage = 'spanish'
-            } else {
-                this.welcome = dynamicContent.english.welcome;
-                this.currentLanguage = 'english'
-            }
-        }, 3000);
     },
     data() {
         return {
-           currentLanguage: 'english',
-           welcome: dynamicContent.english.welcome,
+           variable: 'x',
         }
     },
 }
