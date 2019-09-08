601e89f624ab30e2b2db93ebf0fbda24a7ef4493
Nestor A Hernandez <hernandez_nestor@network.lilly.com>
2019-09-08 13:41:49 -0400

    more detail

--- src/router.js
+++ src/router.js
@@ -8,12 +8,30 @@ export default new Router({
     {
       path: '/',
       name: 'home',
-      component: Home
+      component: Home,
+      meta: {
+        title: 'Home Page - nahdev profile site',
+        metaTags: [
+          {
+            name: 'description',
+            content: 'The home page for nahdev(Nestor A Hernandez Development)'
+          }
+        ]
+      }
     },
     {
       path: '/about',
       name: 'about',
-      component: About
+      component: About,
+      meta: {
+        title: 'About Page - about myself and nahdev',
+        metaTags: [
+          {
+            name: 'description',
+            content: 'Few more details about nestor hernandez'
+          }
+        ]
+      }
     },
     {
       path: '/timelines',
