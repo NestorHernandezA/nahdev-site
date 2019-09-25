26cd1fffb4293672010bab291304dd08ee8fb2ad
Nestor A Hernandez <hernandez_nestor@network.lilly.com>
2019-09-24 21:04:01 -0400

    good start for dynamic cards

--- src/router.js
+++ src/router.js
@@ -1,6 +1,7 @@
 import Router from 'vue-router'
 import Home from './views/Home.vue'
 import About from './views/About.vue'
+import AboutTechnology from './views/AboutTechnology.vue'
 
 export default new Router({
   routes: [
@@ -23,11 +24,25 @@ export default new Router({
       name: 'about',
       component: About,
       meta: {
-        title: 'About Page - about myself and nahdev',
+        title: 'About Page',
         metaTags: [
           {
             name: 'description',
             content: 'Few more details about nestor hernandez'
+          }
+        ]
+      }
+    },
+    {
+      path: '/about-technology',
+      name: 'about-technology',
+      component: AboutTechnology,
+      meta: {
+        title: 'About Technology Page',
+        metaTags: [
+          {
+            name: 'description',
+            content: 'Details on technology and knowledge'
           }
         ]
       }
